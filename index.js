af7257d0c1f4c2b08d864b37945eaa73848b8d17
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-09 01:13:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ function upperCaseCustomerName(){
   customerName = 'BOB';
 }
 function setBestCustomer(){
-    bestCustomer = 'not bob';
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
 }
